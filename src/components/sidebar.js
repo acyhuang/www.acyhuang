8cb80c920ac67597a7c1871001b907048a0cf363
acyhuang <allisoncyhuang@gmail.com>
2024-06-02 00:18:29 -0700

    Created reusable styles

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => (
     <Link to="/about" className="font-gothic-a1 text-gray-200 text-6xl font-bold leading-3.3">allison huang</Link>
     <div className="text-gray-400">
       <p>IS CURRENTLY</p>
-      <ul className="list-disc list-inside">
+      <ul>
           <li>STUDYING AN INTEGRATED DESIGN, CS, AND BUSINESS DEGREE AT UNIVERSITY OF SOUTHERN CALIFORNIA</li>
           <li>READING ___</li>
           <li>TRYING TO FIGURE THE REST OUT...</li>
