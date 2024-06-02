7d06288e5eec9deeaffb882f7103cfb5b859c618
acyhuang <allisoncyhuang@gmail.com>
2024-06-02 15:16:23 -0700

    Added documate content

--- src/components/posts/documate.js
+++ src/components/posts/documate.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PostHeader from '../postHeader';
 
+import simplifyImg from '../../assets/documate/simplify.png';
+import searchImg from '../../assets/documate/search.png';
+import qaImg from '../../assets/documate/qa.png';
+import screensImg from '../../assets/documate/screens.png';
+import macImg from '../../assets/documate/mac.png';
+
 const postDetails = {
   title: 'Documate',
   tag: 'design',
@@ -12,16 +18,26 @@ const Documate = () => {
   return (
     <div className="py-16 px-12">
       <PostHeader {...postDetails} />
-      <p>This was a project completed in USC’s premier product incubator, Lavalab, where we conceived, built, and pitched Documate over eight weeks. This was my first time building a product from 0 to 1, and the process gave me the chance to wear several different hats with the goal of building something that users love.</p>
+      <p>This was a project completed in USC’s premier product incubator, Lavalab, where we conceived, built, and pitched Documate over eight weeks.</p>
       
       <h1>Problem: Hardware documentation is really hard to read.</h1>
       <p>Manuals are full of dense text and diagrams, and they’re often hundreds of pages long. This makes it incredibly time-consuming to figure out how to accomplish a specific task with a piece of unfamiliar hardware.</p>
       
       <h1>Solution: Simplify, search, and query.</h1>
       <p>Documate uses LLMs to parse through documentation, making it easier to navigate and understand. This is done in three ways:</p>
-      <p>Simplify. Information under headers of the document are summarized into one page, allowing for a general understanding of the product in addition to providing a navigation point.</p>
-      <p>Semantic search. Instead of having to know the exact keyword, phrase, or sentence that will bring them to the section they’re looking for, users can search by meaning.</p>
-      <p>Question and answer. Users can as questions to with a chatbot about how to perform a certain function or better understand what a table, diagram, or graph means.</p>
+      <p><b>Simplify.</b> Information under headers of the document are summarized into one page, allowing for a general understanding of the product in addition to providing a navigation point.</p>
+      <div className='w-full flex justify-center'>
+        <img src={simplifyImg} alt="screenshot" className="max-w-lg w-full"></img>
+      </div>
+      <p><b>Semantic search.</b> Instead of having to know the exact keyword, phrase, or sentence that will bring them to the section they’re looking for, users can search by meaning.</p>
+      <div className='w-full flex justify-center'>
+        <img src={searchImg} alt="screenshot" className="max-w-lg w-full"></img>
+      </div>
+      <p><b>Question and answer.</b> Users can as questions to with a chatbot about how to perform a certain function or better understand what a table, diagram, or graph means.</p>  
+      <div className='w-full flex justify-center'>
+        <img src={qaImg} alt="screenshot" className="max-w-lg w-full"></img>
+      </div>
+      
 
       <h1>Process: 0 to 1</h1>
       <p>Because of our cross-functional team, I got the chance to wear multiple hats throughout the process. Aside from the design, I was most involved with the product side.</p>
@@ -49,7 +65,28 @@ const Documate = () => {
 
       <h2>Building features based on the desired benefits</h2>
       <p>Our findings from user interviews translated into concrete features. </p>
-      <p>Desired benefit: Understand the overall function of a piece of hardware. <br></br>Feature: Summary page that compiles the most important information from a manual, e.g. what each button does (simplification).</p>
+      <p><b>Desired benefit:</b> Understand the overall function of a piece of hardware. 
+      <br></br>
+      <b>Feature:</b> Summary page that compiles the most important information from a manual, e.g. what each button does (simplification).</p>
+      <p><b>Desired benefit:</b> Be able to find information about a feature or function even if you don’t know the correct terminology for it.
+      <br></br>
+      <b>Feature:</b> Semantic search allows users to search by meaning, so they’ll get their desired results even if they aren’t using the proper keywords.</p>
+      <p><b>Desired benefit:</b> Quickly figure out whether a certain use case is possible.
+      <br></br>
+      <b>Feature:</b> Q&A feature to allow users to query the manual. All answers cite the respective page number of the manual, so even if the answer isn’t complete, the user now knows where to look.</p>
+      <h2>Implementation: designing to ship fast</h2>
+      <p>This was my first time designing with developers, and we needed to be working closely in order to build out a demo for Demo Day. I learned to build lo-fi wireframes that conveyed the final vision and could be easily built upon by our developers as the prototype reached medium and eventually high fidelity.</p>
+      <p>Looking back, the final handoff of my prototypes to the developers could’ve been significantly improved — while I had high-fidelity prototypes, they lacked some of the information that would’ve been really helpful to developers when building out the designs (ex. how certain spacing should behave when the window size is changed).</p>
+      <p>Regardless I’m proud of our product and grateful for this experience.</p>
+      <div className='w-full flex justify-center'>
+        <img src={screensImg} alt="screenshot" className="max-w-lg w-full"></img>
+      </div>
+
+      <h1>Takeaways</h1>
+      <h2>1. Talk your users. </h2>
+      <p>As someone who had little to no experience with hardware, my understanding of the problem was solely made possible by asking questions, listening, and discussing with potential users. Moreover, conversations revealed how deeply the problem ran. We talked to companies that had made documentation for their own hardware in the form of Google Docs and Slides that weren’t updated regularly. These insights shaped our long term aspirations for the project, though we didn’t end up taking it beyond Lavalab.</p>
+      <h2>2. Prioritize!</h2>
+      <p>Lavalab is only an eight week experience, and we only spent six weeks working as a team on Documate. At any point in time, there was more to do than what we had time to do. We ultimately prioritized the three features our users had expressed a need for, and were able to build those out well enough to demo.</p>
     </div>
   );
 };
