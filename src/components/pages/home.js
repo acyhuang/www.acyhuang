40552d90c2aba56e5f58e9ef9e288d38d9353166
acyhuang <allisoncyhuang@gmail.com>
2024-06-02 17:35:03 -0700

    Updated content

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => (
     <div className='text-container'>
       <h1>helloooo</h1>
-      <p>I'm Allison. A junior at USC, a product-obsessed generalist, and lover of lists.</p>
-      <p>Welcome to my little internet space, </p>
+      <p>I'm Allison — a junior at USC, a product-obsessed generalist, and lover of lists. Welcome to my little internet space. Currently a work in progress. </p>
 
       <h2>other places i exist on the internet</h2>
       <p><a href="https://www.linkedin.com/in/allisoncyhuang/" target="_new">LinkedIn,</a> <a href="https://github.com/acyhuang" target="_new">Github</a></p>
