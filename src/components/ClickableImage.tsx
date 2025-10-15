536637b0515938aa90f296d532ce3ffe5062883a
Allison Huang <allisoncyhuang@gmail.com>
2025-10-15 16:22:22 -0700

    fix type

--- src/components/ClickableImage.tsx
+++ src/components/ClickableImage.tsx
@@ -1,4 +1,4 @@
-import { ImgHTMLAttributes } from 'react'
+import type { ImgHTMLAttributes } from 'react'
 
 interface ClickableImageProps extends ImgHTMLAttributes<HTMLImageElement> {
   onImageClick: (src: string) => void
